46ec5897ad034b1cb09151e16039a851f8be3975
NancyLykins <NancyLykins@proton.me>
2024-01-25 07:29:40 -0300

    Setting client

--- bot/client.py
+++ bot/client.py
@@ -1,16 +1,21 @@
-import discord, cleanCache, asyncio, youtube_dl, checkCharacter, math, os
-from discord import app_commands
+import discord, os
 from discord.ext import commands
 from key import TOKEN
-from cogs.loadCogs import setup_hook
-client = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-@client.event
+CLIENT = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+@CLIENT.event
 async def on_ready():
-    await setup_hook(client)
-    synced = await client.tree.sync()
-    print(f"Syncd {len(synced)} command(s)")
-    await cleanCache.cleanCache(client)
-    print(f"Bot is already started like: {client.user}")
-    
-client.run(TOKEN)
+    print("Starting to load cogs")
+    for root, dirs, files in os.walk("cogs"):
+        for file in files:
+            if not root.endswith("__pycache__"):
+                path = os.path.join(root, file)[:-3].replace("/", ".")
+                print(f"Loading {path}")
+                await CLIENT.load_extension(path)
+    print("All cogs was loaded")
+    synced = await CLIENT.tree.sync()
+    print(f"Synced {len(synced)} command(s)")
+    print(f"Client was started like: {CLIENT.user}")
+
+CLIENT.run(TOKEN)
