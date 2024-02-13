94234bb9f5e9e7bc0c34f2bf04fabd4c6c455f4f
NancyLykins <NancyLykins@proton.me>
2024-02-13 16:21:36 -0300

    Fixing mana response

--- bot/commands/character/characterMana.py
+++ bot/commands/character/characterMana.py
@@ -8,4 +8,4 @@ async def characterMana(interaction, player: discord.Member, mana: int, option):
     body = {"mp": f"{option}{mana}"}
     async with aiohttp.ClientSession() as session:
         async with session.patch(f"{url}/characters/{player.id}", headers=header, json=body) as response:
-            await interaction.response.send_message(f"{mana} {'foram' if mana > 1 else 'foi'} {'removida' if option == '-' else 'restaurada'} de {player}")
+            await interaction.response.send_message(f"{mana} de mana {'foram' if mana > 1 else 'foi'} {'removida' if option == '-' else 'restaurada'} de {player}")
