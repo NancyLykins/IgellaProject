a588632e30c2aa16e28daa9dabc50ef802e73e31
NancyLykins <NancyLykins@proton.me>
2024-02-13 16:20:49 -0300

    Fixing and updating life command

--- bot/commands/character/characterLife.py
+++ bot/commands/character/characterLife.py
@@ -7,4 +7,5 @@ header = {'Content-type': 'application/json'}
 async def characterLife(interaction, player: discord.Member, life: int, option):
     body = {"hp": f"{option}{life}"}
     async with aiohttp.ClientSession() as session:
-        await session.patch(f"{url}/characters/{interaction.user.id}", headers=header, json=body)  
+        async with session.patch(f"{url}/characters/{player.id}", headers=header, json=body) as response:
+            await interaction.response.send_message((f"{life} de vida {'foram' if life > 1 else 'foi'} {'removida' if option == '-' else 'restaurada'} de {player}"))
