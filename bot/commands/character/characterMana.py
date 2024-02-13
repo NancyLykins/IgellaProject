71527b0a95b8bd8564569332ac43695f92cdf756
NancyLykins <NancyLykins@proton.me>
2024-02-13 16:16:08 -0300

    Fixing and updating mana command

--- bot/commands/character/characterMana.py
+++ bot/commands/character/characterMana.py
@@ -7,4 +7,5 @@ header = {'Content-type': 'application/json'}
 async def characterMana(interaction, player: discord.Member, mana: int, option):
     body = {"mp": f"{option}{mana}"}
     async with aiohttp.ClientSession() as session:
-        await session.patch(f"{url}/characters/{interaction.user.id}", headers=header, json=body)  
+        async with session.patch(f"{url}/characters/{player.id}", headers=header, json=body) as response:
+            await interaction.response.send_message(f"{mana} {'foram' if mana > 1 else 'foi'} {'removida' if option == '-' else 'restaurada'} de {player}")
