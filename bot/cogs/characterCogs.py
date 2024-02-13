19e1e6cb762666fc5d33b8a75d3c2453f203d19f
NancyLykins <NancyLykins@proton.me>
2024-02-13 15:56:06 -0300

    Heal charcter command

--- bot/cogs/characterCogs.py
+++ bot/cogs/characterCogs.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 
 from commands.status.showStatus import showStatus
+from commands.character.healCharacter import healCharacter
 
 class characterCogs(commands.Cog):
     def __init__(self, client: discord.client):
@@ -12,6 +13,11 @@ class characterCogs(commands.Cog):
     async def status(self, interaction: discord.Interaction):
         await showStatus(interaction)
         
+    @app_commands.command(name="heal", description="Cura uma certa quantidade de vida de um personagem")
+    @app_commands.describe(player="Quem deve ser curado?")
+    @app_commands.describe(life="Quanto de vida deve ser restaurado?")
+    async def heal(self, interaction, player: discord.Member, life: int):
+        await healCharacter(interaction, player, life)
         
 async def setup(client: discord.Client) -> None:
     await client.add_cog(characterCogs(client))
