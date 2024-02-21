866a4b563d2c2e1bcaa8b94539328fb5fe93ab26
NancyLykins <NancyLykins@proton.me>
2024-02-21 10:59:22 -0300

    Setting the var for url

--- web/server/processData/item.php
+++ web/server/processData/item.php
@@ -7,10 +7,11 @@ $unity = $_POST["unity"];
 $type = $_POST["type"];
 $desc = $_POST["desc"];
 $file = $_FILES["image"];
-echo $url."/test";
-curl_setopt($ch, CURLOPT_URL, $url."/test");
+$url = url."/";
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $name);
 move_uploaded_file($file["tmp_name"], "./uploads/".$file["name"]);
  
+
 ?>
