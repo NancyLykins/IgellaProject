591dd88e13304d2aff80a253855e5dca77d991cb
NancyLykins <NancyLykins@proton.me>
2024-02-21 11:45:08 -0300

    Updating curl statements

--- web/server/apiConnection.php
+++ web/server/apiConnection.php
@@ -1,3 +1,3 @@
 <?php
-    define("url", "http://localhost:5050");
+    define("API_URL", "http://localhost:5050");
 ?>
--- web/server/processData/item.php
+++ web/server/processData/item.php
@@ -1,17 +1,22 @@
 <?php
 include("../apiConnection.php");
-$ch = curl_init();
-$name = $_POST["name"];
-$weight = $_POST["weight"];
-$unity = $_POST["unity"];
-$type = $_POST["type"];
-$desc = $_POST["desc"];
 $file = $_FILES["image"];
-$url = url."/test";
+$url = API_URL."/test";
+
+$data = array(
+    "name" => $_POST["name"],
+    "weight" => $_POST["weight"],
+    "unity" => $_POST["unity"],
+    "type" => $_POST["type"],
+    "desc" => $_POST["desc"],
+    "imgPath" => $_FILES["image"]["name"],
+);
+$ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $name);
-move_uploaded_file($file["tmp_name"], "./uploads/".$file["name"]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+curl_exec($ch);
  
 
 ?>
