2423e75dbaf6fad87c77b6dd5caf17d88b66f01f
NancyLykins <NancyLykins@proton.me>
2024-02-21 13:32:40 -0300

    Change teh path variable

--- api/src/multer.js
+++ api/src/multer.js
@@ -4,7 +4,7 @@ const path = require("path")
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         let filePath = `public/${req.body.path}`
-        callback(null, path.resolve("filePath"));
+        callback(null, path.resolve(filePath));
     },
     filename: (req, file, callback) => {
         const time = new Date().getTime();
