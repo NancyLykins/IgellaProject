9ba291fc44fde62f52c33f16a81071506801b507
nancy lykins <nancy@kali.igella.net>
2024-04-27 18:36:16 -0400

    creating Campaign model and passing de account model to js

---
+++ api/src/models/Campaign.js
@@ -0,0 +1,44 @@
+import { sequelize } from "../config/index.js";
+import pkg from 'sequelize';
+import Account from "./Account.js";
+const { DataTypes } = pkg;
+
+const Campaign = sequelize.define(
+    "campaigns",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        sistem: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        started: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    },
+    {
+        freezeTableName: true,
+        timestamps: false
+    }
+)
+
+Campaign.belongsTo(Account, {
+    as: "cargo",
+    onDelete: 'NO ACTION',
+    onUpdate: 'NO ACTION',
+    foreignKey: {
+        name: 'master',
+        allowNull: false,
+    },
+});
