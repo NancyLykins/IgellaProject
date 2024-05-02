62c1e8f342c8017eb906e4bb6f099c6f383823da
NancyLykins <NancyLykins@proton.me>
2024-05-02 06:03:26 -0400

    updating map element router

--- api/src/routers/mapElementRouter.ts
+++ api/src/routers/mapElementRouter.ts
@@ -3,9 +3,9 @@ import mapElementController from "../controllers/mapElementController";
 
 
 export function mapElementRouter(app: express.Application){
-    app.get("/map/elements", mapElementController.get)
-    app.get("/map/elements/:id", mapElementController.get)
-    app.post("/map/elements", mapElementController.post)
-    app.delete("/map/elements/:id", mapElementController.destroy)
-    app.patch("/map/elements/:id", mapElementController.update)
+    app.get("/maps/elements", mapElementController.get)
+    app.get("/maps/elements/:id", mapElementController.get)
+    app.post("/maps/elements", mapElementController.post)
+    app.delete("/maps/elements/:id", mapElementController.destroy)
+    app.patch("/maps/elements/:id", mapElementController.update)
 }
