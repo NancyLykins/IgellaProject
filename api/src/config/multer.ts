4f8bcdc1b8974cc64472ecab951e2047c9a6c90d
nancy lykins <nancy@kali.igella.net>
2024-05-01 20:39:43 -0400

    re-making multer file

---
+++ api/src/config/multer.ts
@@ -0,0 +1,13 @@
+import multer from "multer"
+import path from "path";
+
+
+export default {
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, "..", "..", "public"),
+        filename(req, file, callback){
+            const time = new Date().getTime();
+            callback(null, `${file.originalname}_${time}`);
+        },
+    })
+}
